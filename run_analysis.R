5f626759a4c027baa07710a116602082882832b4
userdiego <45924054+userdiego@users.noreply.github.com>
2019-01-22 18:53:14 -0300

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,57 @@
+##Downloading and unzipping the data
+
+
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", destfile = "data.zip")
+unzip("data.zip")
+
+##Reading in the raw data
+
+setwd("./UCI HAR Dataset")
+feat <- read.table("features.txt")
+labs <- read.table("activity_labels.txt")
+measures_test <- read.table("./test/X_test.txt")
+measures_train <- read.table("./train/X_train.txt")
+subject_test <- read.table("./test/subject_test.txt")
+subject_train <- read.table("./train/subject_train.txt")
+act_test <- read.table("./test/y_test.txt")
+act_train <- read.table("./train/y_train.txt")
+
+##Adding the subjects and activities to the measurments
+
+data_test <- cbind(subject_test, act_test, measures_test)
+data_train <- cbind(subject_train, act_train, measures_train)
+
+##Merging the test and train
+
+data <- rbind(data_test, data_train)
+
+##Adding the descriptive measurement and activity names to the variables
+
+colnames(data) <- c("subject", "activity", as.character(feat$V2))
+data$activity <- factor(data$activity, levels = c(1:6), labels = c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", "SITTING", "STANDING", "LAYING"))
+names(data) <- gsub("-", "_", names(data))
+names(data) <- gsub("\\(", "", names(data))
+names(data) <- gsub("\\)", "", names(data))
+names(data) <- gsub("^t","Time", names(data))
+names(data) <- gsub("^f","Freq", names(data))
+
+## Extracting means and standard deviation measurements.
+## Ordering by subject.
+
+selection <- c(grep("mean()", names(data)), grep("std()", names(data)))
+data <- data[ , c(1, 2, selection)]
+
+install.packages("dplyr")
+library(dplyr)
+data <- arrange(data, data$subject)
+
+## 
+
+## Creating tidy dataset with variable averages for each subject.
+## Saving dataset in .txt file.
+
+install.packages("reshape2")
+library(reshape2)
+melt_data <- melt(data, id = c("subject", "activity"), measure.vars = c(3:81))
+tidy <- dcast(melt_data, subject + activity ~ variable, mean)
+write.table(tidy, file = "tidy.txt")
